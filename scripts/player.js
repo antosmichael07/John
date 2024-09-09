8be62de54560df101521c2b68e36516327e74b99
Antoš Michael <antosmichael07@gmail.com>
2024-09-09 15:55:15 +0200

    sprinting

--- scripts/player.js
+++ scripts/player.js
@@ -5,46 +5,55 @@ class Player {
         this.scale = 80
         this.cameraX = canvas.width / 2 - this.scale / 2
         this.cameraY = canvas.height / 2 - this.scale / 2
-        this.speed = 3
-        this.diagonalSpeed = 2.121
+        this.speed = 1.5
+        this.diagonalSpeed = 1.060
+        this.sprintSpeed = 2.5
+        this.diagonalSprintSpeed = 1.767
         this.xVelocity = 0
         this.yVelocity = 0
-        this.moveKeysPressed = 0
         this.image = loadImage("./resources/textures/player.png")
     }
 
     update() {
-        this.move()
+        this.movement()
     }
 
-    move() {
+    movement() {
         this.xVelocity = 0
         this.yVelocity = 0
-        this.moveKeysPressed = 0
+        let moveKeysPressed = 0
 
         if (keyInputs["KeyW"]) {
             this.yVelocity -= 1
-            this.moveKeysPressed++
+            moveKeysPressed++
         }
         if (keyInputs["KeyS"]) {
             this.yVelocity += 1
-            this.moveKeysPressed++
+            moveKeysPressed++
         }
         if (keyInputs["KeyA"]) {
             this.xVelocity -= 1
-            this.moveKeysPressed++
+            moveKeysPressed++
         }
         if (keyInputs["KeyD"]) {
             this.xVelocity += 1
-            this.moveKeysPressed++
+            moveKeysPressed++
         }
 
-        if (this.moveKeysPressed != 2) {
-            this.x += this.xVelocity * this.speed
-            this.y += this.yVelocity * this.speed
+        if (!keyInputs["ShiftLeft"]) {
+            this.move(moveKeysPressed, this.speed, this.diagonalSpeed)
         } else {
-            this.x += this.xVelocity * this.diagonalSpeed
-            this.y += this.yVelocity * this.diagonalSpeed
+            this.move(moveKeysPressed, this.sprintSpeed, this.diagonalSprintSpeed)
+        }
+    }
+
+    move(moveKeysPressed, speed, diagonalSpeed) {
+        if (moveKeysPressed != 2) {
+            this.x += this.xVelocity * speed
+            this.y += this.yVelocity * speed
+        } else {
+            this.x += this.xVelocity * diagonalSpeed
+            this.y += this.yVelocity * diagonalSpeed
         }
     }
 
