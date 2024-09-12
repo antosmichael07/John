aa2cd89a37113b7eed1cf2848b42c7e364e3aeca
Michael Antoš <128068126+antosmichael07@users.noreply.github.com>
2024-09-12 09:52:21 +0000

    fixed player drawing

--- scripts/utilities.js
+++ scripts/utilities.js
@@ -11,5 +11,5 @@ function clearBackground(color) {
 }
 
 function drawRelativeToPlayer(image, x, y) {
-    c.drawImage(image, x - player.x + 605, y - player.z + 310)
+    c.drawImage(image, x - player.x + 605, y - player.z + 305)
 }
