2000e0f505f98e4f731f17ea65c7eada4361edd0
Antoš Michael <antosmichael07@gmail.com>
2024-09-29 09:00:27 +0200

    a level now has a width of 2048

--- scripts/level.js
+++ scripts/level.js
@@ -1,8 +1,9 @@
 class Level {
     constructor () {
+        // The levels have max width 2048 and max height 12 (24KB)
         this.data = [
-            new Uint8Array(512), new Uint8Array(512), new Uint8Array(512), new Uint8Array(512), new Uint8Array(512), new Uint8Array(512), 
-            new Uint8Array(512), new Uint8Array(512), new Uint8Array(512), new Uint8Array(512), new Uint8Array(512), new Uint8Array(512)
+            new Uint8Array(2048), new Uint8Array(2048), new Uint8Array(2048), new Uint8Array(2048), new Uint8Array(2048), new Uint8Array(2048), 
+            new Uint8Array(2048), new Uint8Array(2048), new Uint8Array(2048), new Uint8Array(2048), new Uint8Array(2048), new Uint8Array(2048)
         ];
     }
 
